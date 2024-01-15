82cd6d2ce248a3fd0ccbbbc8cbdda4a70477a47b
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2024-01-15 16:15:30 +0100

    feat: added image and name to main div with some css

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,11 +4,23 @@ import image from '../../assets/vin1.png';
  
 export const Home = () => {
     return (
+        <> 
         <div className='home-body'>
             <div className='title-home'>
-                <div className='home'><img className=' home' src= {image} alt="home" />  </div> 
+                <div className='home-image'><img className='home-image' src= {image} alt="home" />  </div> 
                 <div className='title-name'>Vincenzo Donnarumma</div>
             </div>
         </div>
+        <div className='about-me'>
+
+        </div>
+        <div className='projects'>
+
+        </div>
+        <div className='contact'>
+
+        </div>
+
+        </>
     );
 };
