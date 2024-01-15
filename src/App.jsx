8f258b66c630cf4926cbb1133855fda1401b7bf5
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2024-01-15 14:30:31 +0100

    Created portfolio folders

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Body } from './pages/Body/Body'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <> 
+      <Body />
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+import image from '../../assets/vin1.png';
+ 
+export const Home = () => {
+    return (
+        <div className='home-body'>
+            <div className='title-home'>
+                <div className='home'><img className=' home' src= {image} alt="home" />  </div> 
+            </div>
+        </div>
+    );
+};
