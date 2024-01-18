1ac6e293e3b768a8edc43def018e801a5cdc6293
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2024-01-18 12:23:40 +0100

    feat: added button to downdload cv

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -31,7 +31,7 @@ import linkedIn from '../../assets/LnkdIn.png';
 import malcriada from '../../assets/malcriada.png';
 import mastermind from '../../assets/mastermind.png';
 import videoconsola from '../../assets/videoconsola.png';
-import geekzone from '../../assets/geekzone.png'; 
+import geekzone from '../../assets/geekzone.png';
 
 export const Home = () => {
 
@@ -48,7 +48,7 @@ export const Home = () => {
         <>
             <div className='home-body'>
                 <div className='title-home'>
-                    <div   draggable='true'><img className='home-image' src={image} alt='home' />  </div>
+                    <div draggable='true'><img className='home-image' src={image} alt='home' />  </div>
                     <div className='title-name'>Vincenzo Donnarumma</div>
                     <div className='button-home' onClick={() => handleScrollToSection(about)}>DESCUBRE MÁS</div>
                 </div>
@@ -188,6 +188,20 @@ export const Home = () => {
             </div>
             <div className='contact'>
                 <div className='message'>¡Hagamos algo genial!</div>
+                <div class="Download-button">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="16"
+                        width="20"
+                        viewBox="0 0 640 512"
+                    >
+                        <path
+                            d="M144 480C64.5 480 0 415.5 0 336c0-62.8 40.2-116.2 96.2-135.9c-.1-2.7-.2-5.4-.2-8.1c0-88.4 71.6-160 160-160c59.3 0 111 32.2 138.7 80.2C409.9 102 428.3 96 448 96c53 0 96 43 96 96c0 12.2-2.3 23.8-6.4 34.6C596 238.4 640 290.1 640 352c0 70.7-57.3 128-128 128H144zm79-167l80 80c9.4 9.4 24.6 9.4 33.9 0l80-80c9.4-9.4 9.4-24.6 0-33.9s-24.6-9.4-33.9 0l-39 39V184c0-13.3-10.7-24-24-24s-24 10.7-24 24V318.1l-39-39c-9.4-9.4-24.6-9.4-33.9 0s-9.4 24.6 0 33.9z"
+                            fill="white"
+                        ></path>
+                    </svg>
+                    <div className='cv-text'>Descargar CV</div>
+                </div>
                 <div className='box-contact'>
                     <div className='left-contact'>
                         <div className='contact-title'>CONTACTO</div>
