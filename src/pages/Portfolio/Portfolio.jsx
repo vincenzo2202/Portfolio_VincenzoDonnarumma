6667b409458781100b2b49bee832d7e5ea04e1b3
vincenzo2202 <vincenzodonnarumma22@gmail.com>
2024-01-22 13:21:09 +0100

    feat: added button to go back home

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -9,15 +9,40 @@ import diagramaMalcriada from '../../assets/diagrama-malcriada.png'
 import mastermind from '../../assets/mastermind.png'
 import videoconsola from '../../assets/videoconsola.png'
 import gardenButcher from '../../assets/garden-butcher.png'
+import { useNavigate } from 'react-router-dom';
 
 
 export const Portfolio = () => {
+    const navigate = useNavigate()
 
+    const homeBtn = ()=>{
+        return navigate('/');
+     }
 
 
     return (
         <div className="portfolio-body">
+            <div className='title-box'>
+            <div class="button" onClick={homeBtn}> 
+                <div class="button-box">
+                    <span class="button-elem">
+                        <svg viewBox="0 0 46 40" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M46 20.038c0-.7-.3-1.5-.8-2.1l-16-17c-1.1-1-3.2-1.4-4.4-.3-1.2 1.1-1.2 3.3 0 4.4l11.3 11.9H3c-1.7 0-3 1.3-3 3s1.3 3 3 3h33.1l-11.3 11.9c-1 1-1.2 3.3 0 4.4 1.2 1.1 3.3.8 4.4-.3l16-17c.5-.5.8-1.1.8-1.9z"
+                            ></path>
+                        </svg>
+                    </span>
+                    <span class="button-elem">
+                        <svg viewBox="0 0 46 40">
+                            <path
+                                d="M46 20.038c0-.7-.3-1.5-.8-2.1l-16-17c-1.1-1-3.2-1.4-4.4-.3-1.2 1.1-1.2 3.3 0 4.4l11.3 11.9H3c-1.7 0-3 1.3-3 3s1.3 3 3 3h33.1l-11.3 11.9c-1 1-1.2 3.3 0 4.4 1.2 1.1 3.3.8 4.4-.3l16-17c.5-.5.8-1.1.8-1.9z"
+                            ></path>
+                        </svg>
+                    </span>
+                </div>
+            </div>
             <div className="portfolio-title">PORTFOLIO</div>
+            </div>
             <div className="content-portfolio">
                 <PortfolioCard
                     cardImage={geekZone}
